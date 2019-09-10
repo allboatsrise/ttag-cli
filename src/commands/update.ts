c599595d040f891990ea869369873f63a2192e24
Andrej Pavlovic <andrej.pavlovic@massedge.com>
2019-09-10 16:55:44 -0400

    commands/update - sort messages after new and old po data is merged

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -3,7 +3,7 @@ import * as ttagTypes from "../types";
 import * as fs from "fs";
 import { extractAll } from "../lib/extract";
 import { updatePo } from "../lib/update";
-import { parse } from "../lib/parser";
+import { parse, Messages } from "../lib/parser";
 import { serialize } from "../lib/serializer";
 
 async function update(
@@ -21,6 +21,21 @@ async function update(
         const po = parse(fs.readFileSync(pofile).toString());
         console.log(pot, po);
         const resultPo = updatePo(pot, po);
+
+        // sort by message id if enabled
+        if (ttagOverrideOpts && ttagOverrideOpts.sortByMsgid) {
+            const ctxs = Object.keys(resultPo.translations);
+            for (const ctx of ctxs) {
+                const oldPoEntries = resultPo.translations[ctx];
+                const newPoEntries: Messages = {};
+                const keys = Object.keys(oldPoEntries).sort();
+                keys.forEach(k => {
+                    newPoEntries[k] = oldPoEntries[k];
+                });
+                resultPo.translations[ctx] = newPoEntries;
+            }
+        }
+
         fs.writeFileSync(pofile, serialize(resultPo));
         progress.succeed(`${pofile} updated`);
     } catch (err) {
